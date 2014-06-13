8a777ef4ee048112bdd8972c9a5d80a4671e9ed7
Florin Godard <fgodard@gmail.com>
2014-06-13 12:35:19 +0000

    Fixed bug in regexp which ignored all numbers >14999

--- krautcounter.sh
+++ krautcounter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 date +"%s" | tr -d '\n'
 echo "	" | tr -d '\n'
-wget https://krautreporter.de/das-magazin -O - 2>/dev/null | grep -A 1 "project--progress-info--text" | grep -oP -m 1 "1[1-4][0-9]{3}"
+wget https://krautreporter.de/das-magazin -O - 2>/dev/null | grep -A 1 "project--progress-info--text" | grep -oP -m 1 "[0-9]{5}" | head -n 1
 
